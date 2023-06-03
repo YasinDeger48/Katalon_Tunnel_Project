f6e53785481e0e3897a35b5239486e3ee1550d45
yasindegerV <yasin.deger@virgosol.com>
2023-06-03 10:01:22 +0300

    suite created

---
+++ Test Suites/LoginTestSuiteForTunnel.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>LoginTestSuiteForTunnel</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>7d4f7924-638c-409e-acae-b2e0f4672ad2</testSuiteGuid>
+</TestSuiteEntity>
